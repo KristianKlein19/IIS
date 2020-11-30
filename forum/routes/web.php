78ebe8b53d9a2234cd7bdbff5951cec1b974a4aa
Andrej Pavlovič <xpavlo14@vutbr.cz>
2020-11-30 20:28:24 +0100

    edit

--- forum/routes/web.php
+++ forum/routes/web.php
@@ -42,7 +42,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/groups/{id}/requests', [App\Http\Controllers\RequestController::class, 'index'])->name('group.requests');
 
-    Route::get('/group/{id1}/thread/{id2}', [App\Http\Controllers\ThreadController::class, 'index'])->name('thread');
+    Route::get('/groups/{id1}/thread/{id2}', [App\Http\Controllers\ThreadController::class, 'index'])->name('thread');
 
     Route::get('/request/{id}/reject', [App\Http\Controllers\RequestController::class, 'reject'])->name('request.reject');
 
