629562df94a8dd054ae31090807b33cab2e24723
BrainFuck42 <Kubamorlo@seznam.cz>
2020-12-01 19:22:33 +0100

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(50)->create();
-        \App\Models\Skupina::factory(10)->create();
-        \App\Models\Vlakno::factory(50)->create();
-        \App\Models\Prispevek::factory(250)->create();
-        \App\Models\Zadost::factory(10)->create();
-        \App\Models\Clen::factory(100)->create();
-        \App\Models\Hodnotil::factory(1000)->create();
-        \App\Models\Moderator::factory(20)->create();
+        //\App\Models\User::factory(50)->create();
+        //\App\Models\Skupina::factory(10)->create();
+        //\App\Models\Vlakno::factory(50)->create();
+        //\App\Models\Prispevek::factory(250)->create();
+        //\App\Models\Zadost::factory(10)->create();
+        //\App\Models\Clen::factory(100)->create();
+        //\App\Models\Hodnotil::factory(1000)->create();
+        //\App\Models\Moderator::factory(20)->create();
         $this->call(TestUserSeeder::class);
     }
 }
